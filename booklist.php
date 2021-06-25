4389a755c24a09777f48cb358c9b10d49a362ab7
BaeSungMin <qo98333@naver.com>
2021-06-25 10:01:08 +0900

    로그인 구현

--- booklist.php
+++ booklist.php
@@ -18,63 +18,63 @@ echo("에러 내용: ".$e -> getMessage());
 ?>
 
 <!DOCTYPE html>
-<html lang="ko">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
-integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0"
-crossorigin="anonymous">
-<style>
-a {
-text-decoration: none;
-}
-</style>
-<title>BOOK LIST</title>
-</head>
-<body>
+    <html lang="ko">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <!-- Bootstrap CSS -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" 
+            rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 
-<div class="container">
-<h2 class="text-center">Book List</h2>
-<table class="table table-bordered text-center">
-<thead>
-<tr>
-<th>제목</th>
-<th>출판사</th>
-<th>가격</th>
-</tr>
-</thead>
-<tbody>
-<?php
-$stmt = $conn -> prepare("SELECT BOOK_ID, BOOK_NAME, PUBLISHER, PRICE FROM BOOK WHERE LOWER(BOOK_NAME) LIKE '%' || :searchWord || 
-'%' ORDER BY BOOK_NAME");
-$stmt -> execute(array($searchWord));
-while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-?>
-<tr>
-<td><a href="bookview.php?bookId=<?= $row['BOOK_ID'] ?>"><?= $row['BOOK_NAME'] ?></a></td>
-<td><?= $row['PUBLISHER'] ?></td>
-<td><?= $row['PRICE'] ?></td>
-</tr>
-<?php
-}
-?>
+            <style>
+            a { text-decoration: none; }
+            </style>
+            <title>BOOK LIST</title>
+        </head>
+        
+        <body>
+            <div class="container">
+                <h2 class="text-center">Book List</h2> <!--제목 -->
+                
+                <table class="table table-bordered text-center">
+                <thead>
+                    <tr>
+                        <th>제목</th> <!-- 표지 -->
+                        <th>출판사</th>
+                        <th>가격</th>
+                    </tr>
+                </thead>
+                <tbody> <!--책목록 구현 -->
+                    <?php
+                        $stmt = $conn -> prepare("SELECT BOOK_ID, BOOK_NAME, PUBLISHER, PRICE FROM BOOK WHERE LOWER(BOOK_NAME) LIKE '%' || :searchWord || '%' ORDER BY BOOK_NAME");
+                        $stmt -> execute(array($searchWord));
+                        while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+                    ?>
+                    <tr>
+                        <td><a href="bookview.php?bookId=<?= $row['BOOK_ID'] ?>"><?= $row['BOOK_NAME'] ?></a></td>
+                        <td><?= $row['PUBLISHER'] ?></td>
+                        <td><?= $row['PRICE'] ?></td>
+                    </tr>
+                    <?php
+                        }
+                    ?>
+                </tbody>
+                </table>
 
-</tbody>
-</table>
-<div class="d-grid d-md-flex justify-content-md-end">
-<a href="input.php?mode=insert" class="btn btn-primary">등록</a>
+            <div class="d-grid d-md-flex justify-content-md-end"> <!--입력 단추용 -->
+                <a href="input.php?mode=insert" class="btn btn-primary">등록</a> 
+            </div>
+    
+        <form class="row">
+            <div class="col-10"> <!--검색어 입력용 -->
+                <label for="searchWord" class="visually-hidden">Search Word</label>
+                <input type="text" class="form-control" id="searchWord" name="searchWord" placeholder="검색어 입력" value="<?= $searchWord ?>">
+            </div>
+
+            <div class="col-auto text-end"> <!--검색 단추용 -->
+                <button type="submit" class="btn btn-primary mb-3">검색</button>
+            </div>
+        </form>
 </div>
-<form class="row">
-<div class="col-10">
-<label for="searchWord" class="visually-hidden">Search Word</label>
-<input type="text" class="form-control" id="searchWord" name="searchWord" placeholder="검색어 입력" value="<?= 
-$searchWord ?>">
-</div>
-<div class="col-auto text-end">
-<button type="submit" class="btn btn-primary mb-3">검색</button>
-</div>
-</form>
-</div>
-</body></html>
+</body>
+</html>
---
+++ forlogin/login.php
@@ -0,0 +1,50 @@
+<!-- 로그인 페이지(시작 페이지) -->
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <title>회원가입 및 로그인 사이트</title>
+    <link rel="stylesheet" type="text/css" href="common.css" />
+
+    <style>
+    h1 { text-align : center;}
+    </style>
+  </head>
+  
+  <body>
+    <h1>배성민 서점</h1><br></br>  <!-- -->
+    <p style="text-align:center;"><img src = "Lib.jpeg"></p><br></br>
+    
+    <div id="login_box"> 						
+			<form method="post" action="/forlogin/login_ok.php"> <!-- 로그인 박스 누르면 login_ok.php로 이동 -->
+				<table align="center" border="0" cellspacing="0" width="300"> <!-- 로그인 박스 주변 테두리 -->
+        			<tr>
+            			<td width="130" colspan="0"> 
+                		<input type="text" name="userid" class="inph"> <!-- 로그인 입력 -->
+                  		</td>
+
+                		<td rowspan="2" align="center" width="100" > 
+                		<button type="submit" id="btn" >로그인</button>
+                		</td>
+        	    	</tr>
+
+        		    <tr>
+            	    	<td width="130" colspan="1"> 
+               		    <input type="password" name="userpw" class="inph">  <!-- 비밀번호 입력 -->
+            	        </td>
+        	        </tr>
+                  <!--
+                 	<tr>
+           		        <td colspan="3" align="center" class="mem"> 
+              	        <a href="member.php">회원가입 하시겠습니까?</a>
+                        </td>
+                    </tr>
+                  -->
+                </table>
+            </form>
+    </div>
+    
+  </body>
+</html>
